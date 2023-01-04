3f0a4a93cd0b12355143472bd937c473a0c18533
真_人工智障 <67090055+TotoWang-hhh@users.noreply.github.com>
2023-01-04 16:25:14 +0800

    v1.0.0代码上传+文档页导航栏官网链接修改
    更新内容见文档
    其次是把文档页中，本人官网的链接修改到了新域名

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-with open("./README.md", "r") as fh:
+with open("./README.md", "r",encoding='utf-8') as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
+with open('./requirements.txt','r',encoding='utf-8') as f:
     requirements = [l for l in f.read().splitlines() if l]
 
 setup(name="tttk",  # 项目名
-      version="0.3.2",       # 版本号
+      version="1.0.0",       # 版本号
       description="tkinter的增强库",  #简介
       long_description=long_description,  # 长简介 这里使用的 readme 内容
       long_description_content_type="text/markdown",
-      license="MPL",   # 授权
+      license="MPL 2.0",   # 授权
       install_requires=requirements, # 依赖
-      author="真_人工智障",  # 作者
+      author="真_人工智障（rgzz666）",  # 作者
       author_email="tt1224@hotmail.com",  # 邮箱
       url="https://github.com/TotoWang-hhh/tttk/",  # 地址
       download_url="https://github.com/TotoWang-hhh/tttk/archive/master.zip",
-      packages=find_packages(),
+      packages=["tttk"],
       keywords=['tttk','tkinter增强','tk增强'],
       zip_safe=True)
